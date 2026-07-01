133ffdd3fe4df7cd094be1128bc7f753cbfb804d
kjh010914 <kjh010914@gmail.com>
2026-07-02 07:41:18 +0900

    Update 4M_Project.py

--- 4M_Project.py
+++ 4M_Project.py
@@ -48,7 +48,7 @@ if menu == "📱 현장 변동점 등록":
         st.markdown("👤 **등록자 정보**")
         col_dept, col_name = st.columns(2)
         with col_dept:
-            department = st.text_input("🏢 부서", placeholder="예: 생산1팀") # 🌟 흐름글씨 적용
+            department = st.text_input("🏢 부서", placeholder="예: 생산팀") # 🌟 흐름글씨 적용
         with col_name:
             worker_name = st.text_input("🧑‍💼 담당자 성명", placeholder="예: 홍길동") # 🌟 흐름글씨 적용
         
